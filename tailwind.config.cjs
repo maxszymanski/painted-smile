9cf07112754ecc80c7103cfaa5f7eb141b89473c
Maksymilian Szymański <maksymilianszymanski2@gmail.com>
2023-10-09 22:21:16 +0200

    create and styled link component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,17 @@ module.exports = {
             fontFamily: {
                 sans: ['Atma', ...defaultTheme.fontFamily.sans],
             },
+            backgroundImage: {
+                'bg-small': "url('/src/images/header/lines-small.webp')",
+                'bg-big': "url('/src/images/header/lines-big.webp')",
+            },
+            colors: {
+                mypink: '#FF8DA6',
+                myblue: '#107F9B',
+                mygrey: '#E5EEF0',
+                mylightblue: '#BBF3FF',
+                mylightpink: '#F86F8D',
+            },
         },
     },
     plugins: [],
