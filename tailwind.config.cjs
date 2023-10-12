bfa22dac6531f434cac69debdc9026040fcadf7f
Maksymilian Szymański <maksymilianszymanski2@gmail.com>
2023-10-12 19:45:42 +0200

    end offers section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ['Atma', ...defaultTheme.fontFamily.sans],
+                love: 'Love Ya Like A Sister, sans-serif',
             },
             backgroundImage: {
                 'bg-small': "url('/src/images/header/lines-small.webp')",
@@ -18,6 +19,7 @@ module.exports = {
                 'hover-green': "url('/src/images/nav/hover-green.png')",
                 'hover-pink': "url('/src/images/nav/hover-pink.png')",
                 'hover-blue': "url('/src/images/nav/hover-blue.png')",
+                'more-btn': "url('/src/images/cards/more-btn.png')",
             },
             colors: {
                 mypink: '#FF8DA6',
@@ -29,6 +31,11 @@ module.exports = {
                 mygreyblue: '#3DA352',
                 myspiritblue: '#05A8D2',
                 mycardblue: '#F8FEFF',
+                myorder: '#86D4E8',
+                cardpink: '#CF284D',
+                cardyellow: '#FFBF1D',
+                cardred: '#C92424',
+                cardorange: '#FF8D2B',
             },
         },
     },
