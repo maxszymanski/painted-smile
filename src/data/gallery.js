bab79f98acee1f7d51b8c17cf0e6c0904de26677
Maksymilian Szymański <maksymilianszymanski2@gmail.com>
2023-10-21 11:54:47 +0200

    change images to server rendering

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,218 +1,339 @@
+import foto1 from '../images/images/foto1.png'
+import foto2 from '../images/images/foto2.png'
+import foto3 from '../images/images/foto3.png'
+import foto4 from '../images/images/foto4.png'
+import foto6 from '../images/images/foto6.png'
+import foto7 from '../images/images/foto7.png'
+import foto8 from '../images/images/foto8.png'
+import foto10 from '../images/images/foto10.png'
+import foto11 from '../images/images/foto11.png'
+import foto12 from '../images/images/foto12.png'
+import foto15 from '../images/images/foto15.png'
+import foto17 from '../images/images/foto17.png'
+import foto20 from '../images/images/foto20.png'
+import foto21 from '../images/images/foto21.png'
+import foto22 from '../images/images/foto22.png'
+import foto23 from '../images/images/foto23.png'
+import foto24 from '../images/images/foto24.png'
+import foto25 from '../images/images/foto25.png'
+import foto27 from '../images/images/foto27.png'
+import foto28 from '../images/images/foto28.png'
+import foto29 from '../images/images/foto29.png'
+import foto30 from '../images/images/foto30.png'
+import foto31 from '../images/images/foto31.png'
+import foto32 from '../images/images/foto32.png'
+import foto33 from '../images/images/foto33.png'
+import foto35 from '../images/images/foto35.png'
+import foto36 from '../images/images/foto36.png'
+import foto37 from '../images/images/foto37.png'
+import foto38 from '../images/images/foto38.png'
+import foto40 from '../images/images/foto40.png'
+import foto41 from '../images/images/foto41.png'
+import foto43 from '../images/images/foto43.png'
+import foto44 from '../images/images/foto44.png'
+import foto46 from '../images/images/foto46.png'
+import foto47 from '../images/images/foto47.png'
+import foto48 from '../images/images/foto48.png'
+import foto49 from '../images/images/foto49.png'
+import foto53 from '../images/images/foto53.png'
+import foto54 from '../images/images/foto54.png'
+import foto55 from '../images/images/foto55.png'
+import foto56 from '../images/images/foto56.png'
+import foto57 from '../images/images/foto57.png'
+import foto58 from '../images/images/foto58.png'
+import foto59 from '../images/images/foto59.png'
+import foto62 from '../images/images/foto62.png'
+import foto63 from '../images/images/foto63.png'
+import foto64 from '../images/images/foto64.png'
+import foto65 from '../images/images/foto65.png'
+import big14 from '../images/images/big14.png'
+import big16 from '../images/images/big16.png'
+import big18 from '../images/images/big18.png'
+import big19 from '../images/images/big19.png'
+import big26 from '../images/images/big26.png'
+import big34 from '../images/images/big34.png'
+import big39 from '../images/images/big39.png'
+import big45 from '../images/images/big45.png'
+import big50 from '../images/images/big50.png'
+import big51 from '../images/images/big51.png'
+import big52 from '../images/images/big52.png'
+import big61 from '../images/images/big61.png'
+
 const fotos = [
     [
         {
             id: 4,
+            foto: foto4,
         },
         {
             id: 28,
+            foto: foto28,
         },
         {
             id: 26,
             is: true,
+            foto: big26,
         },
         {
             id: 31,
+            foto: foto31,
         },
         {
             id: 32,
+            foto: foto32,
         },
     ],
     [
         {
             id: 16,
             is: true,
+            foto: big16,
         },
         {
             id: 15,
+            foto: foto15,
         },
         {
             id: 23,
+            foto: foto23,
         },
         {
             id: 24,
+            foto: foto24,
         },
         {
             id: 41,
+            foto: foto41,
         },
     ],
     [
         {
             id: 36,
+            foto: foto36,
         },
         {
             id: 22,
+            foto: foto22,
         },
         {
             id: 34,
             is: true,
+            foto: big34,
         },
         {
             id: 37,
+            foto: foto37,
         },
         {
             id: 27,
+            foto: foto27,
         },
     ],
     [
         {
             id: 50,
             is: true,
+            foto: big50,
         },
         {
             id: 48,
+            foto: foto48,
         },
         {
             id: 55,
+            foto: foto55,
         },
         {
             id: 53,
+            foto: foto53,
         },
         {
             id: 58,
+            foto: foto58,
         },
     ],
     [
         {
             id: 63,
+            foto: foto63,
         },
         {
             id: 57,
+            foto: foto57,
         },
         {
             id: 61,
             is: true,
+            foto: big61,
         },
         {
             id: 56,
+            foto: foto56,
         },
         {
             id: 47,
+            foto: foto47,
         },
     ],
     [
         {
             id: 45,
             is: true,
+            foto: big45,
         },
         {
             id: 54,
+            foto: foto54,
         },
         {
             id: 62,
+            foto: foto62,
         },
         {
             id: 64,
+            foto: foto64,
         },
         {
             id: 3,
+            foto: foto3,
         },
     ],
     [
         {
             id: 11,
+            foto: foto11,
         },
         {
             id: 10,
+            foto: foto10,
         },
         {
             id: 18,
             is: true,
+            foto: big18,
         },
         {
             id: 59,
+            foto: foto59,
         },
         {
             id: 40,
+            foto: foto40,
         },
     ],
     [
         {
             id: 51,
             is: true,
+            foto: big51,
         },
         {
             id: 46,
+            foto: foto46,
         },
         {
             id: 20,
+            foto: foto20,
         },
         {
             id: 21,
+            foto: foto21,
         },
         {
             id: 8,
+            foto: foto8,
         },
     ],
     [
         {
             id: 44,
+            foto: foto44,
         },
         {
             id: 65,
+            foto: foto65,
         },
         {
             id: 39,
             is: true,
+            foto: big39,
         },
         {
             id: 12,
+            foto: foto12,
         },
         {
             id: 25,
+            foto: foto25,
         },
     ],
     [
         {
             id: 19,
             is: true,
+            foto: big19,
         },
         {
             id: 30,
+            foto: foto30,
         },
         {
             id: 49,
+            foto: foto49,
         },
         {
             id: 17,
+            foto: foto17,
         },
         {
             id: 35,
+            foto: foto35,
         },
     ],
     [
         {
             id: 2,
+            foto: foto2,
         },
         {
             id: 7,
+            foto: foto7,
         },
         {
             id: 14,
             is: true,
+            foto: big14,
         },
         {
             id: 33,
+            foto: foto33,
         },
         {
             id: 29,
+            foto: foto29,
         },
     ],
     [
         {
             id: 52,
             is: true,
+            foto: big52,
         },
         {
             id: 6,
+            foto: foto6,
         },
         {
             id: 43,
+            foto: foto43,
         },
         {
             id: 38,
+            foto: foto38,
         },
         {
             id: 1,
+            foto: foto1,
         },
     ],
 ]
